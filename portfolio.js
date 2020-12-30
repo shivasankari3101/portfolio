7b3c86fd2291125858aadebd0b7ded2d25414b47
SHIVA SANKARI C <shivasankari3101@gmail.com>
2020-12-30 22:33:57 +0530

    Initial Setup

---
+++ portfolio.js
@@ -0,0 +1,21 @@
+var content =document.getElementById("main_div");
+function showhome(){
+    content.style.transform="translateY(0)";
+    content.style.transition="0.5s";
+}
+function showabout(){
+    content.style.transform="translateY(-100%)";
+    content.style.transition="0.5s";
+}
+function showskills(){
+    content.style.transform="translateY(-200%)";
+    content.style.transition="0.5s";
+}
+function showwork(){
+    content.style.transform="translateY(-300%)";
+    content.style.transition="0.5s";
+}
+function showcontact(){
+    content.style.transform="translateY(-400%)";
+    content.style.transition="0.5s";
+}
