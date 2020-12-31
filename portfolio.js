46d38f74681237c9855eb24902b249361102e138
SHIVA SANKARI C <shivasankari3101@gmail.com>
2020-12-31 09:56:49 +0530

    Added two pages

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,19 @@
 var content =document.getElementById("main_div");
+var sidebar =document.getElementById("sidebar");
+var main_content =document.getElementById("main_content");
+var n=0;
+function showsidebar(){
+   if(n==0){
+    sidebar.style.left="0";
+    main_content.style.left="20%";
+    n=1;
+   }
+   else{
+    sidebar.style.left="-20%";
+    main_content.style.left="0";
+    n=0;
+   }
+}
 function showhome(){
     content.style.transform="translateY(0)";
     content.style.transition="0.5s";
